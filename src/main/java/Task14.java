5c8543896b1262844046105825a328f2effceb9b
MariKrasnykh <mary.krasnyh.99@gmail.com>
2026-02-27 02:57:39 +0300

    lesson 1

---
+++ src/main/java/Task14.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Task14 {
+    public static int[] createArray(int len, int initialValue){
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++)
+            array[i] = initialValue;
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] a = createArray(7,3);
+        System.out.println(Arrays.toString(a));
+    }
+}
